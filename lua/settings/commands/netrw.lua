62d458a8591e64a1cff4aaf84afd1a8df4c6745e
prothegee <prothegee@proton.me>
2025-09-05 13:57:22 +0700

    update 20250905

--- lua/settings/commands/netrw.lua
+++ lua/settings/commands/netrw.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_user_command("NetrwDir", function()
     end
 
     -- netrw configs
-    vim.cmd("vertical leftabove 18Lex")
+    vim.cmd("vertical leftabove 30Lex")
     vim.cmd("setlocal nowrap nonumber norelativenumber")
 
     -- toggle banner
