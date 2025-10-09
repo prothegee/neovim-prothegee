7705e1acab4a2d74514d81fb294ddce2cbc9499d
prothegee <prothegee@proton.me>
2025-10-09 16:32:06 +0700

    updating 20251009

--- lua/nvim-prt/snippets/cpp.lua
+++ lua/nvim-prt/snippets/cpp.lua
@@ -1,53 +1,57 @@
 return {
-    int_main = [[
+
+_comment_spacer_c_based = [[
+// --------------------------------------------------------- // ]],
+
+_head_guard_h = [[
+#ifndef $1_H
+#define $1_H
+#endif // $1_H]],
+
+_head_guard_hh = [[
+#ifndef $1_HH
+#define $1_HH
+#endif // $1_HH]],
+
+_head_guard_hpp = [[
+#ifndef $1_HPP
+#define $1_HPP
+#endif // $1_HPP]],
+
+_namespace_cpp = [[
+namespace $1 {
+// TODO
+} // namespace $1]],
+
+_enum_c_based = [[
+enum $1 {
+    // TODO
+}; // enum $1]],
+
+_enum_class_cpp = [[
+enum class $1 {
+    // TODO
+}; // enum class $1]],
+
+_struct_c_based = [[
+struct $1 {
+    // TODO
+}; // struct $1]],
+
+_class_cpp = [[
+class $1 {
+public:
+    $1();
+    ~$1();
+}; // class $1]],
+
+_int_main_c_based = [[
 int main() {
     return 0;
-}
-]],
-    int_main_args = [[
+}]],
+
+_int_main_argcv_c_based = [[
 int main(int argc, char* argv[]) {
     return 0;
-}
-]],
-
-    for_i = [[
-for (int i = 0; i < var; i++) {
-    // TODO
-}
-]],
-
-    struct_t = [[
-struct Struct_t {
-    // TODO
-}; // Struct_t
-]],
-
-    enum_e = [[
-enum Enum_e {
-    // TODO
-} // Enum_e
-]],
-
-    include_guard = [[
-#ifndef THIS_FILE_HH
-#define THIS_FILE_HH
-#endif // THIS_FILE_HH
-]],
-
-    class_t = [[
-class Class_t {
-pubic:
-    Class_t();
-   ~Class_t();
-}; // Class_t
-]],
-
-    namespace_n = [[
-namespace namespace_n {
-} // namespace namespace_n
-]],
-
-    comment_spacer = [[
-// --------------------------------------------------------------- //
-]]
+}]],
 }
