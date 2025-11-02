68e33e78e9adbc532b3e916363a2d103584a8e3d
prothegee <prothegee@proton.me>
2025-11-02 12:09:20 +0700

    adding next tab command

--- lua/settings/keymaps/tab.lua
+++ lua/settings/keymaps/tab.lua
@@ -2,6 +2,10 @@ local function create_empty_new_tab()
     vim.cmd("tabnew")
 end
 
+local function toggle_next_tab()
+    vim.cmd("tabnext")
+end
+
 ---
 
 -- open new tab
@@ -16,6 +20,16 @@ vim.keymap.set(
     create_empty_new_tab,
     {
         desc = "create empty new tab (mode: n, i, v, t)"
+    }
+)
+
+-- togle new tab
+vim.keymap.set(
+    { "n", "i", "v", "t" },
+    "<C-tab>",
+    toggle_next_tab,
+    {
+        desc = "toggle next tab (mode: n, i, v, t)"
     }
 )
 
