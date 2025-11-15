a6c26365574be763e03c60a5c7c05e2063676c84
prothegee <prothegee@proton.me>
2025-11-15 19:36:16 +0700

    adding hlchunk

---
+++ lua/plugins/hlchunk.lua
@@ -0,0 +1,20 @@
+local _hlchunk = require"hlchunk"
+
+_hlchunk.setup({
+    chunk = {
+        enable = true,
+    },
+    indent = {
+        enable = true
+    },
+    line_num = {
+        enable = true
+    },
+    blank = {
+        enable = false
+    },
+    context = {
+        enable = false
+    }
+})
+
